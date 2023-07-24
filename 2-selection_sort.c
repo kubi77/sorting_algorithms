09c713c53adc46b201336ecc72df207b0733f5c2
kubi77 <ac51262@gmail.com>
2023-07-24 08:19:37 -0700

    sorting out algorithms

---
+++ 2-selection_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * selection_sort - Sort an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
+void selection_sort(int *array, size_t size)
+{
+	int *min;
+	size_t j, k;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (j = 0; j < size - 1; j++)
+	{
+		min = array + j;
+		for (k = j + 1; k < size; k++)
+			min = (array[k] < *min) ? (array + k) : min;
+
+		if ((array + j) != min)
+		{
+			swap_ints(array + j, min);
+			print_array(array, size);
+		}
+	}
+}
+
---
+++ vim
@@ -0,0 +1,3 @@
+Best Case: O(n^2) (The number of comparisons and swaps will be the same as the average and worst cases.)
+Average Case: O(n^2) (The number of comparisons and swaps is proportional to n^2, where n is the size of the array.)
+Worst Case: O(n^2) (The number of comparisons and swaps is proportional to n^2, where n is the size of the array.)
